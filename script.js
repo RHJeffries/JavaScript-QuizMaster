8538a2ef8fb88f7efc78ea7ae83778221d6bfe39
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-09-26 12:33:14 +0930

    updated start button to start timer. Added styling

--- script.js
+++ script.js
@@ -1,85 +1,127 @@
+var startButton = document.querySelector(".start-button");
+
+
+
+function startGame() {
+
+    startButton.setAttribute("style", "hidden");
+    startButton.disabled = true;
+    //askQuestions()
+    timer()
+}
+
+
 //timer
 var timerEl = document.getElementById("timer");
 
-function timer() {
-var timeRemain = 30;
 
-var timeLeft = setInterval(function ()  {
-    
-    if (timeRemain > 1) {
-        timerEl.textContent = timeRemain;
-        timeRemain--;
-    } else {
-        timerEl.textContent = '';
-        clearInterval(timeLeft);
-    }
-}, 1000);
 
+
+function timeOut() {
+    popQuiz.textContent = "Game Over";
+   
+    //number correct plus time remaining for score
+    //startButton.disabled = false;
+    //highScore()
 }
-timer();
 
-// Quiz questions, each an object
+function timer() {
+    
+    var timeRemain = 10;
+    var timeLeft = setInterval(function () {
+        
+        if (timeRemain > 0) {
+            timerEl.textContent = timeRemain;
+            timeRemain--;
+        }
+        if (timeRemain === 0) {
+            clearInterval(timeLeft);
+            timeOut();
+        }
+    }, 1000);
+}
+
+
+
+
+
+// Quiz questions, each question an object
+var listEl = document.createElement("ul");
+
 
 var questions = [
-{
-    question: "What are variables used for in JavaScript Programs?",
-    answers: {
-        a: "Storing numbers, dates, or other values",
-        b: "Varying randomly",
-        c: "Undefined",
-        d: "A method for choosing a value"
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        answers: {
+            a: "Storing numbers, dates, or other values",
+            b: "Varying randomly",
+            c: "Undefined",
+            d: "A method for choosing a value"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-},
-{
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: {
-        a: "<js>",
-        b: "<scripting>",
-        c: "<script>",
-        d: "<javascript>"
+    /*{
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<js>",
+            b: "<scripting>",
+            c: "<script>",
+            d: "<javascript>"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-},
-{
-    question: "An Array is a...",
-    answers: {
-        a: "Ordered list of values",
-        b: "Unordered list of objects",
-        c: "Weapon on the Death Star",
-        d: "String of characters"
+    {
+        question: "An Array is a...",
+        answers: {
+            a: "Ordered list of values",
+            b: "Unordered list of objects",
+            c: "Weapon on the Death Star",
+            d: "String of characters"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "Which function is used to round a number down?",
-    answers: {
-        a: "Math.random",
-        b: "Math.floor",
-        c: "Else If",
-        d: "Malfunction"
+    {
+        question: "Which function is used to round a number down?",
+        answers: {
+            a: "Math.random",
+            b: "Math.floor",
+            c: "Else If",
+            d: "Malfunction"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "Where does the push method add a new item to an array?",
-    answers: {
-        a: "Beginning",
-        b: "Middle",
-        c: "Random",
-        d: "End"
+    {
+        question: "Where does the push method add a new item to an array?",
+        answers: {
+            a: "Beginning",
+            b: "Middle",
+            c: "Random",
+            d: "End"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-},
-{
-    question: "A for loop will execute -",
-    answers: {
-        a: "As long as a condition is true",
-        b: "As long as a condition is false",
-        c: "Endlessly",
-        d: "Not at all"
-    },
-    correctAnswer: "a"
-}
+    {
+        question: "A for loop will execute -",
+        answers: {
+            a: "As long as a condition is true",
+            b: "As long as a condition is false",
+            c: "Endlessly",
+            d: "Not at all"
+        },
+        correctAnswer: "a"
+    }*/
 
 ];
+console.log(questions)
+
+var popQuiz = document.querySelector("#pop-quiz"); 
+var chooseQuestions = "";
+function askQuestions(){
+chooseQuestions = questions[Math.floor(Math.random() * questions.length)];
+popQuiz.textContent = chooseQuestions("");
+}
+console.log(chooseQuestions)
+
+//questions.textContent = "";
+ // click start button to being quiz and start timer
+startButton.addEventListener("click", startGame);
